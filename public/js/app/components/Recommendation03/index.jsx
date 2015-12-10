bd4563bc16753a7584c3a953fe0355190eaecdb8
blackChef <hongpidai@gmail.com>
2015-12-10 19:59:08 +0800

    make android legacy work

--- public/js/app/components/Recommendation03/index.jsx
+++ public/js/app/components/Recommendation03/index.jsx
@@ -10,6 +10,8 @@ var Elem = React.createClass({
   },
 
   componentDidMount: function() {
+    var container = this.refs.contentContainer;
+
     function afterScriptExecute(cb) {
 
       // 广告脚本执行完之后会把 window.tosAdspaceInfo 设为 null
@@ -24,7 +26,6 @@ var Elem = React.createClass({
       loop();
     }
 
-    var container = this.refs.contentContainer;
     function loadScript(id, onload = function() {}) {
       window.tosAdspaceInfo = _.assign({}, __tosAdspaceInfo, { aid: id });
       var script = document.createElement('script');
@@ -35,15 +36,31 @@ var Elem = React.createClass({
       container.appendChild(script);
     }
 
+
+    // 在 android legacy browser 动态插入脚本时，
+    // 不论脚本插入到哪个元素内，元素的 parentNode 总是返回 body
+    function fixAndroidLegacy() {
+      if ( document.documentElement.classList.contains('no-flexbox') &&
+           document.documentElement.classList.contains('flexboxlegacy') ) {
+        var ins = document.querySelectorAll('body > ins');
+        _.forEach(ins, function(item) {
+          container.appendChild(item);
+        });
+      }
+    }
+
     var ids = this.props.payload.map( (item) => +item.text );
     function load() {
       var i = 0;
       function loop() {
-        if (i <= ids.length - 1) {
+        if (i < ids.length - 1) {
           loadScript(ids[i], function() {
             i += 1;
             loop();
           });
+        } else if (i == ids.length - 1) {
+          // all script executed
+          loadScript(ids[i], fixAndroidLegacy);
         }
       }
 
